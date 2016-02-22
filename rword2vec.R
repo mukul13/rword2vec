e31af9f6341391a1ad4269a38b11b17e98ada47c
mukul13 <mukul.chaware13@gmail.com>
2016-02-22 22:28:24 +0530

    word2vec first commit

---
+++ rword2vec.R
@@ -0,0 +1,32 @@
+dyn.load("word2vec.so")
+
+word2vec=function(layer1_size=0,train_file=0,save_vocab_file=0,read_vocab_file=0,
+                  debug_mode=0,binary=0,cbow=0,alpha=0,output_file=0,window=0,
+                  sample=0,hs=0,negative=0, num_threads=0,iter=0,min_count=0,classes=0)
+{
+  OUT = .C("word2vec",rlayer1_size=as.character(layer1_size),
+           rtrain_file=as.character(train_file),
+           rsave_vocab_file=as.character(save_vocab_file),
+           rread_vocab_file=as.character(read_vocab_file),
+           rdebug_mode=as.character(debug_mode),
+           rbinary=as.character(binary),
+           rcbow=as.character(cbow),
+           ralpha=as.character(alpha),
+           routput_file=as.character(output_file),
+           rwindow=as.character(window),
+           rsample=as.character(sample),
+           rhs=as.character(hs),
+           rnegative=as.character(negative),
+           rnum_threads=as.character(num_threads),
+           riter=as.character(iter),
+           rmin_count=as.character(min_count),
+           rclasses=as.character(classes)
+           )
+  class(OUT) <- "word2vec"
+  names(OUT)[2] <- "model_file"
+  OUT
+}
+
+
+model=word2vec(train_file = "text8",output_file = "vector.txt",binary=0)
+#model=word2vec(train_file = "text8",output_file = "vec.bin",binary=1)
