7903d94e3a284dc36d72478aed153e8fbb6caa5b
mukul13 <mukul.chaware13@gmail.com>
2016-02-23 22:10:35 +0530

    distance and analogy function added

--- rword2vec.R
+++ rword2vec.R
@@ -1,7 +1,9 @@
+setwd("/home/mukul/Desktop/Mukul Backup/deep learning/word2vec")
+
 dyn.load("word2vec.so")
 
 word2vec=function(layer1_size=0,train_file=0,save_vocab_file=0,read_vocab_file=0,
-                  debug_mode=0,binary=0,cbow=0,alpha=0,output_file=0,window=0,
+                  debug_mode=0,binary=1,cbow=0,alpha=0,output_file=0,window=0,
                   sample=0,hs=0,negative=0, num_threads=0,iter=0,min_count=0,classes=0)
 {
   OUT = .C("word2vec",rlayer1_size=as.character(layer1_size),
@@ -28,5 +30,24 @@ word2vec=function(layer1_size=0,train_file=0,save_vocab_file=0,read_vocab_file=0
 }
 
 
-model=word2vec(train_file = "text8",output_file = "vector.txt",binary=0)
-#model=word2vec(train_file = "text8",output_file = "vec.bin",binary=1)
+#model=word2vec(train_file = "text8",output_file = "vector.txt",binary=0)
+model=word2vec(train_file = "text8",output_file = "vec.bin",binary=1)
+
+dyn.load("distance.so")
+
+distance=function(file_name,search_word,num=15)
+{
+  OUT=.C("distance",rfile_name=as.character(file_name),search_word=as.character(search_word),rN=as.integer(num),rbestw=as.character(rep("123",num)),rbestd=as.double(rep(0,num)))  
+  as.data.frame(cbind(OUT$rbestw,OUT$rbestd))
+}
+
+dist=distance("vec.bin","king",20)
+
+dyn.load("word_analogy.so")
+word_analogy=function(file_name,search_words,num=20)
+{
+  OUT=.C("word_analogy",rfile_name=as.character(file_name),search_words=as.character(search_words),rN=as.integer(num),rbestw=as.character(rep("11",num)),rbestd=as.double(rep(0,num)))
+  as.data.frame(cbind(OUT$rbestw,OUT$rbestd))
+}
+
+ana=word_analogy("vec.bin","king queen man",20)
